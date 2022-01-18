431ca15feb17564f7eaeb506233c8277ea186479
V260570 <39130778+V260570@users.noreply.github.com>
2022-01-18 20:03:55 +0700

    Update test3.ts

--- test3.ts
+++ test3.ts
@@ -3,12 +3,12 @@ interface HoleObject {
 }
 
 // создание массива случайных чисел
-const arr = Array(100).fill(0).map(() => Math.floor(Math.random() * 100000));
+const arr = Array(50).fill(0).map(() => Math.floor(Math.random() * 10000));
 // числа с "дырками"
 const holes: HoleObject = {0: 1, 4: 1, 6: 1, 9: 1, 8: 2};
 
 // функция для сортировки, которая возвращает количество "дырок" в числе
-const el = (a: number): number => [...a + ''].map((v: string): number => Object.keys(holes).includes(v) ? holes[v] : 0).reduce((t, c) => t + c);
+const el = (a: number): number => [...a + ''].map((v: string): number => holes[v] || 0).reduce((t, c) => t + c);
 const sortedArray = arr.sort((a, b) => a - b).sort((a, b) => el(a) - el(b));
 
 console.log(sortedArray);
