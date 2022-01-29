cde2af5580ddffb5e325d6ca8060102b30dc38f2
V260570 <39130778+V260570@users.noreply.github.com>
2022-01-29 16:16:08 +0700

    Update test3.ts

--- test3.ts
+++ test3.ts
@@ -9,6 +9,6 @@ const holes: HoleObject = {0: 1, 4: 1, 6: 1, 9: 1, 8: 2};
 
 // функция для сортировки, которая возвращает количество "дырок" в числе
 const el = (a: number): number => [...a + ''].map((v: string): number => holes[v] || 0).reduce((t, c) => t + c);
-const sortedArray = arr.sort((a, b) => a - b).sort((a, b) => el(a) - el(b));
+const sortedArray = arr.sort((a, b) => el(a) - el(b) || a - b);
 
 console.log(sortedArray);
